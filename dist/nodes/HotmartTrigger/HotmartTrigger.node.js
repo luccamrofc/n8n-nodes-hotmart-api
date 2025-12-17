d529e0edbb732a6b552505077e1a350d85fa434f
Lucca Maia <lucmairod2@gmail.com>
2025-12-17 10:02:24 -0300

    Correções de layout do node nos modos flow e super flow, adição da possibilidade de mudar o nome das saídas e o path do webhook trigger node.

--- dist/nodes/HotmartTrigger/HotmartTrigger.node.js
+++ dist/nodes/HotmartTrigger/HotmartTrigger.node.js
@@ -16,27 +16,27 @@ const FLOW_EVENT_MAP = {
     'SWITCH_PLAN': 11,
 };
 const FLOW_OUTPUT_NAMES = [
-    'Compra Aprovada',
-    'Compra Completa',
-    'Compra Cancelada',
-    'Compra Reembolsada',
-    'Chargeback',
-    'Boleto Impresso',
-    'Compra Atrasada',
-    'Compra Expirada',
-    'Abandono Carrinho',
-    'Disputa Aberta',
-    'Cancel. Assinatura',
-    'Troca de Plano',
-    'Outros',
+    { type: 'main', displayName: 'Compra Aprovada' },
+    { type: 'main', displayName: 'Compra Completa' },
+    { type: 'main', displayName: 'Compra Cancelada' },
+    { type: 'main', displayName: 'Compra Reembolsada' },
+    { type: 'main', displayName: 'Chargeback' },
+    { type: 'main', displayName: 'Boleto Impresso' },
+    { type: 'main', displayName: 'Compra Atrasada' },
+    { type: 'main', displayName: 'Compra Expirada' },
+    { type: 'main', displayName: 'Abandono Carrinho' },
+    { type: 'main', displayName: 'Disputa Aberta' },
+    { type: 'main', displayName: 'Cancel. Assinatura' },
+    { type: 'main', displayName: 'Troca de Plano' },
+    { type: 'main', displayName: 'Outros' },
 ];
 const SUPER_FLOW_OUTPUT_NAMES = [
-    'Compra Única',
-    'Nova Assinatura',
-    'Renovação',
-    'Cancelamento',
-    'Problema Pagamento',
-    'Outros',
+    { type: 'main', displayName: 'Compra Única' },
+    { type: 'main', displayName: 'Nova Assinatura' },
+    { type: 'main', displayName: 'Renovação' },
+    { type: 'main', displayName: 'Cancelamento' },
+    { type: 'main', displayName: 'Problema Pagamento' },
+    { type: 'main', displayName: 'Outros' },
 ];
 function getFlowOutputIndex(eventType) {
     var _a;
@@ -150,9 +150,30 @@ class HotmartTrigger {
             inputs: [],
             outputs: `={{
             $parameter["webhookMode"] === "flow" 
-                ? ${JSON.stringify(FLOW_OUTPUT_NAMES)}
+                ? [
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_approved"] : "Compra Aprovada" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_complete"] : "Compra Completa" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_canceled"] : "Compra Cancelada" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_refunded"] : "Compra Reembolsada" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_chargeback"] : "Chargeback" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_billet"] : "Boleto Impresso" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_delayed"] : "Compra Atrasada" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_expired"] : "Compra Expirada" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_abandoned"] : "Abandono Carrinho" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_dispute"] : "Disputa Aberta" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_sub_cancel"] : "Cancel. Assinatura" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_plan_switch"] : "Troca de Plano" },
+                    { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_flow_other"] : "Outros" },
+                ]
                 : $parameter["webhookMode"] === "superFlow"
-                    ? ${JSON.stringify(SUPER_FLOW_OUTPUT_NAMES)}
+                    ? [
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_single"] : "Compra Única" },
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_new_sub"] : "Nova Assinatura" },
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_renewal"] : "Renovação" },
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_cancellation"] : "Cancelamento" },
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_payment"] : "Problema Pagamento" },
+                        { type: 'main', displayName: $parameter["customizeOutputs"] ? $parameter["output_super_other"] : "Outros" },
+                    ]
                     : ["main"]
         }}`,
             webhooks: [
@@ -160,7 +181,7 @@ class HotmartTrigger {
                     name: 'default',
                     httpMethod: 'POST',
                     responseMode: 'onReceived',
-                    path: 'webhook',
+                    path: '={{ $parameter["path"] ? $parameter["path"] : "webhook" }}',
                 },
             ],
             properties: [
@@ -293,6 +314,249 @@ class HotmartTrigger {
                     },
                     default: '',
                     description: 'Opcional: O segredo Hottok das configurações de webhook da Hotmart. Se configurado, requisições sem um hottok válido serão rejeitadas.',
+                },
+                {
+                    displayName: 'Path Customizado',
+                    name: 'path',
+                    type: 'string',
+                    default: 'webhook',
+                    placeholder: 'webhook',
+                    description: 'O caminho da URL do webhook. Padrão é "webhook".',
+                },
+                {
+                    displayName: 'Personalizar Nomes das Saídas',
+                    name: 'customizeOutputs',
+                    type: 'boolean',
+                    default: false,
+                    description: 'Ative para renomear as saídas dos modos Flow e Super Flow',
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Única',
+                    name: 'output_super_single',
+                    type: 'string',
+                    default: 'Compra Única',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Nova Assinatura',
+                    name: 'output_super_new_sub',
+                    type: 'string',
+                    default: 'Nova Assinatura',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Renovação',
+                    name: 'output_super_renewal',
+                    type: 'string',
+                    default: 'Renovação',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Cancelamento',
+                    name: 'output_super_cancellation',
+                    type: 'string',
+                    default: 'Cancelamento',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Problema Pagamento',
+                    name: 'output_super_payment',
+                    type: 'string',
+                    default: 'Problema Pagamento',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Outros',
+                    name: 'output_super_other',
+                    type: 'string',
+                    default: 'Outros',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['superFlow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Aprovada',
+                    name: 'output_flow_approved',
+                    type: 'string',
+                    default: 'Compra Aprovada',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Completa',
+                    name: 'output_flow_complete',
+                    type: 'string',
+                    default: 'Compra Completa',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Cancelada',
+                    name: 'output_flow_canceled',
+                    type: 'string',
+                    default: 'Compra Cancelada',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Reembolsada',
+                    name: 'output_flow_refunded',
+                    type: 'string',
+                    default: 'Compra Reembolsada',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Chargeback',
+                    name: 'output_flow_chargeback',
+                    type: 'string',
+                    default: 'Chargeback',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Boleto Impresso',
+                    name: 'output_flow_billet',
+                    type: 'string',
+                    default: 'Boleto Impresso',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Atrasada',
+                    name: 'output_flow_delayed',
+                    type: 'string',
+                    default: 'Compra Atrasada',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Compra Expirada',
+                    name: 'output_flow_expired',
+                    type: 'string',
+                    default: 'Compra Expirada',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Abandono Carrinho',
+                    name: 'output_flow_abandoned',
+                    type: 'string',
+                    default: 'Abandono Carrinho',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Disputa Aberta',
+                    name: 'output_flow_dispute',
+                    type: 'string',
+                    default: 'Disputa Aberta',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Cancel. Assinatura',
+                    name: 'output_flow_sub_cancel',
+                    type: 'string',
+                    default: 'Cancel. Assinatura',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Troca de Plano',
+                    name: 'output_flow_plan_switch',
+                    type: 'string',
+                    default: 'Troca de Plano',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
+                },
+                {
+                    displayName: 'Nome alternativo para: Outros',
+                    name: 'output_flow_other',
+                    type: 'string',
+                    default: 'Outros',
+                    displayOptions: {
+                        show: {
+                            webhookMode: ['flow'],
+                            customizeOutputs: [true],
+                        },
+                    },
                 },
             ],
         };
